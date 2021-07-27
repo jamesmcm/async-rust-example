eab692b7afc0cce9a7977274763e8d78c525f925
Chris Shenkan <cshenkan7@gmail.com>
2021-07-26 23:40:42 -0400

    fixed server to run on current version of tokio

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,31 +1,27 @@
-use futures::stream::StreamExt;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     let addr = "127.0.0.1:6142";
-    let mut listener = TcpListener::bind(addr).await.unwrap();
+    let listener = TcpListener::bind(addr).await.unwrap();
 
     let server = {
         async move {
-            let mut incoming = listener.incoming();
-            while let Some(conn) = incoming.next().await {
-                match conn {
-                    Err(e) => eprintln!("accept failed = {:?}", e),
-                    Ok(mut sock) => {
-                        tokio::spawn(async move {
-                            let (mut reader, mut writer) = sock.split();
-                            tokio::time::delay_for(tokio::time::Duration::from_secs(8)).await;
-                            match tokio::io::copy(&mut reader, &mut writer).await {
-                                Ok(amt) => {
-                                    println!("wrote {} bytes", amt);
-                                }
-                                Err(err) => {
-                                    eprintln!("IO error {:?}", err);
-                                }
+            match listener.accept().await {
+                Err(e) => eprintln!("accept failed = {:?}", e),
+                Ok((mut sock, _addr)) => {
+                    tokio::spawn(async move {
+                        let (mut reader, mut writer) = sock.split();
+                        tokio::time::sleep(tokio::time::Duration::from_secs(8)).await;
+                        match tokio::io::copy(&mut reader, &mut writer).await {
+                            Ok(amt) => {
+                                println!("wrote {} bytes", amt);
                             }
-                        });
-                    }
+                            Err(err) => {
+                                eprintln!("IO error {:?}", err);
+                            }
+                        }
+                    });
                 }
             }
         }
